2f4756557b1c8ae2cbfbeea1e7451383ac6d45fa
min0209 <ssyj0209@gmail.com>
2021-04-06 12:07:57 +0900

    03-04 POST : create PostController

---
+++ study/src/main/java/com/example/study/Controller/PostController.java
@@ -0,0 +1,33 @@
+package com.example.study.Controller;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.study.model.SearchParam;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+	@PostMapping("/postMethod")
+	public SearchParam postMethod(@RequestBody SearchParam searchParam) {
+		
+		return searchParam;
+		
+	}
+	
+	@PutMapping
+	public void put() {
+		
+	}
+	
+	@PatchMapping
+	public void patch() {
+		
+	}
+
+}

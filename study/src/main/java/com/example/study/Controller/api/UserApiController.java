65a70c58ee695aa1d1d342f89c64a1b1ac82b438
min0209 <ssyj0209@gmail.com>
2021-04-23 16:50:41 +0900

    03-28-1-2 User API create and read

--- study/src/main/java/com/example/study/Controller/api/UserApiController.java
+++ study/src/main/java/com/example/study/Controller/api/UserApiController.java
@@ -1,31 +1,54 @@
 package com.example.study.controller.api;
 
 import com.example.study.controller.ifs.CrudInterface;
+import com.example.study.model.entity.User;
 import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.repository.UserRepository;
+import com.example.study.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
-    public Header create() {
+    @PostMapping("")
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+
+        log.info("{}",request);
+        return  userApiLogicService.create(request);
+
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+
+
         return null;
     }
 
     @Override
-    public Header read(Long id) {
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
         return null;
     }
 
     @Override
-    public Header update() {
+    public Header<UserApiResponse> delete(Long id) {
         return null;
     }
 
-    @Override
-    public Header delete(Long id) {
-        return null;
-    }
+
 }

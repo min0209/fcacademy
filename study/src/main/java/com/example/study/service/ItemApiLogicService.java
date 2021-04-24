1049d61bc3533f11a93bf01dcc746c6e9b049c4a
min0209 <ssyj0209@gmail.com>
2021-04-24 14:18:52 +0900

    03-28-2-02 item API CREATE

---
+++ study/src/main/java/com/example/study/service/ItemApiLogicService.java
@@ -0,0 +1,73 @@
+package com.example.study.service;
+
+import com.example.study.controller.ifs.CrudInterface;
+import com.example.study.model.entity.Item;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.repository.ItemRepository;
+import com.example.study.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ItemApiLogicService implements CrudInterface<ItemApiRequest,ItemApiResponse> {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Override
+    public Header<ItemApiResponse> create(Header<ItemApiRequest> request) {
+        ItemApiRequest body = request.getData();
+
+        Item item = Item.builder()
+                .status(body.getStatus())
+                .name(body.getName())
+                .title(body.getTitle())
+                .content(body.getContent())
+                .price(body.getPrice())
+                .brandName(body.getBrandName())
+                .registeredAt(LocalDateTime.now())
+                .partner(partnerRepository.getOne(body.getPartnerId()))
+                .build();
+        Item newItem = itemRepository.save(item);
+
+        return response(newItem);
+    }
+
+    @Override
+    public Header read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header update(Header request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<ItemApiResponse> response(Item item){
+
+        ItemApiResponse body = ItemApiResponse.builder()
+                .id(item.getId())
+                .status(item.getStatus())
+                .name(item.getName())
+                .title(item.getTitle())
+                .content(item.getContent())
+                .price(item.getPrice())
+                .brandName(item.getBrandName())
+                .registeredAt(item.getRegisteredAt())
+                .partnerId(item.getPartner().getId())
+                .build();
+        return Header.OK(body);
+    }
+}
